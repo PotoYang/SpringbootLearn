85a29282e3b6b4cd4477767710daf2fc34eadc3b
Potoyang <potoyang@163.com>
2019-03-07 00:01:03 +0800

    add unit-test-sample

---
+++ unit-test-sample/src/main/java/com/potoyang/learn/unittestsample/UnitTestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.potoyang.learn.unittestsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestSampleApplication.class, args);
+    }
+
+}
